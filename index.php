359c6bbc999f7d829b874538230055ec4786a285
Kaerv <k.kordal10@gmail.com>
2020-03-14 15:12:45 +0000

    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Black Line</title>
+</head>
+<body>
+    <h1>Hej Kochanie <3</h1>
+</body>
+</html>
