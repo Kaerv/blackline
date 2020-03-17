5f4d9cf9ff38c6f8a5a6606fab56b374c28d46e9
Kaerv <k.kordal10@gmail.com>
2020-03-17 11:18:29 +0000

    Move links style to general.css

--- templates_c/b54d3fd2b30ca9d8e85d261cbd9270e81dfdd535_0.file.quote_section.tpl.php
+++ templates_c/b54d3fd2b30ca9d8e85d261cbd9270e81dfdd535_0.file.quote_section.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.34-dev-7, created on 2020-03-17 11:00:20
+/* Smarty version 3.1.34-dev-7, created on 2020-03-17 11:17:55
   from '/var/www/html/templates/quote_section.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.34-dev-7',
-  'unifunc' => 'content_5e70adc49b3d90_46479564',
+  'unifunc' => 'content_5e70b1e312a7d2_26562441',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'b54d3fd2b30ca9d8e85d261cbd9270e81dfdd535' => 
     array (
       0 => '/var/www/html/templates/quote_section.tpl',
-      1 => 1584442819,
+      1 => 1584443604,
       2 => 'file',
     ),
   ),
@@ -20,33 +20,40 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5e70adc49b3d90_46479564 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5e70b1e312a7d2_26562441 (Smarty_Internal_Template $_smarty_tpl) {
 ?><section id="quote_section">
     <div id="quotation-link"><a href="#cytaty">CYTATY Z GŁĘBI STRON</a></div>
+
     <header id="quotation-header">
         <div class="header-line"></div>
+
         <div id="quotation-title">
             <div id="quotation-title1">Słowa są ukrytą siłą życia...</div>
             <div id="quotation-title2">ILE MASZ JEJ W SOBIE?</div>
         </div>
+
         <div class="header-line"></div>
     </header>
+
     <div id="quotes">
         <div class="quote-container">
             <div class="quote-sign">,,</div>
             <div class="quote">Kto walczy, może przegrać. Kto nie walczy, już przegrał.</div>
             <div class="quote-author">- BERTOLD BRECHT -</div>
         </div>
+
         <div class="quote-container">
             <div class="quote-sign">,,</div>
             <div class="quote">Talent rośnie w samotności, charakter wśród ludzi.</div>
             <div class="quote-author">- JOHANN WOLFGANG VON GOETHE -</div>
         </div>
+
         <div class="quote-container">
             <div class="quote-sign">,,</div>
             <div class="quote">Mowa jest źródłem nieporozumień.</div>
             <div class="quote-author">- ANTOINE DE SAINT EXUPÉRY -</div>
         </div>
+
         <div class="quote-container">
             <div class="quote-sign">,,</div>
             <div class="quote">[...] zła najczęściej nie da się naprawić; możemy je tylko pomścić.</div>
